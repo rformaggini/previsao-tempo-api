0e66d6128f60535f7004c45b72e483f890c224af
Rodney Formaggini <rformaggini@gmail.com>
2020-09-01 00:44:40 -0300

    Setup Typescript e ESLINT

---
+++ src/util/module-alias.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import { addAliases } from 'module-alias';
+
+
+const files = path.resolve(__dirname, '../..')
+
+addAliases({
+    '@src': path.join(files,'src'),
+    '@test': path.join(files,'test')
+})
