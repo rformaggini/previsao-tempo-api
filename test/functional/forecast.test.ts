170c3340939c276162451e548a68f36959f3651f
Rodney Formaggini <rformaggini@gmail.com>
2020-09-09 13:11:21 -0300

    inclusão do teste

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -2,7 +2,7 @@ import supertest from "supertest";
 
 describe('Beach forecast functional tests', () => {
   it('should return a forecast with just a few times', async() => {
-    const { body, status } = await supertest(app).get('/forecast');
+    const { body, status } = await supertest('').get('/forecast');
     expect(status).toBe(200);
     expect(body).toBe([{
       "time": "2020-04-26T00:00:00+00:00",
