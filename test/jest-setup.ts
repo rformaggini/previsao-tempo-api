0b41e059322eac14f82a70e7276acd1e32598ab6
Rodney Formaggini <rformaggini@gmail.com>
2020-09-10 13:50:18 -0300

    Ajustes do arquivo de Jest.config.js

---
+++ test/jest-setup.ts
@@ -0,0 +1,9 @@
+import { SetupServer } from "@src/server";
+import supertest, { SuperTest } from "supertest";
+
+
+beforeAll(() => {
+    const server = new SetupServer()
+    server.init()
+    global.testRequest = supertest(server.getApp())
+})
